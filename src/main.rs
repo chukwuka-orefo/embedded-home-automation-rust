aac4767b5b5e505fdc02cd185ac18b3d3544450f
Chuks Orefo <c.steve.orefo@gmail.com>
2020-11-16 21:50:28 +0000

    Wire controller with stub devices in main

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
 // embedded-presence-lighting-rust\src\main.rs
 
-mod controller;
+use controller::controller::Controller;
+use controller::state_machine::PresenceLightingStateMachine;
+use interfaces::stub_devices::*;
 
-fn main() {}
+mod controller;
+mod interfaces;
+
+fn main() {
+    let motion = StubMotionSensor::new();
+    let light = StubAmbientLightSensor::new(true);
+    let actuator = StubLightActuator::new();
+
+    let fsm = PresenceLightingStateMachine::default();
+    let mut controller = Controller::new(fsm, motion, light, actuator);
+
+    controller.handle_motion_event();
+    controller.tick();
+}
