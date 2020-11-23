fd53b95ffcdbbd34610639432f5e6db84d89d769
Chuks Orefo <c.steve.orefo@gmail.com>
2020-11-23 21:18:08 +0000

    Fix state machine integration tests to use public API

--- tests/state_machine.rs
+++ tests/state_machine.rs
@@ -1,17 +1,14 @@
 // embedded-presence-lighting-rust\tests\state_machine.rs
 
-use std::time::{Duration, Instant};
-
 use embedded_presence_lighting::controller::state_machine::{
     PresenceLightingStateMachine, State,
 };
 
 #[test]
 fn idle_motion_low_light_turns_on() {
-    let t0 = Instant::now();
     let mut fsm = PresenceLightingStateMachine::new(300, 10);
 
-    let turned_on = fsm.on_motion_detected_at(true, t0);
+    let turned_on = fsm.on_motion_detected(true);
 
     assert!(turned_on);
     assert_eq!(fsm.get_state(), State::LightOn);
@@ -19,18 +16,17 @@ fn idle_motion_low_light_turns_on() {
 
 #[test]
 fn light_turns_off_after_timeout_and_cooldown_expires() {
-    let t0 = Instant::now();
-    let mut fsm = PresenceLightingStateMachine::new(5, 2);
+    // Use zero durations to avoid sleeps in integration tests.
+    let mut fsm = PresenceLightingStateMachine::new(0, 0);
 
-    assert!(fsm.on_motion_detected_at(true, t0));
+    assert!(fsm.on_motion_detected(true));
     assert_eq!(fsm.get_state(), State::LightOn);
 
-    let t_timeout = t0 + Duration::from_secs(5);
-    assert!(fsm.tick_at(t_timeout));
+    // First tick transitions LightOn -> Cooldown and returns true (turn off).
+    assert!(fsm.tick());
     assert_eq!(fsm.get_state(), State::Cooldown);
 
-    let t_done = t_timeout + Duration::from_secs(2);
-    fsm.tick_at(t_done);
-
+    // Second tick transitions Cooldown -> Idle.
+    assert!(!fsm.tick());
     assert_eq!(fsm.get_state(), State::Idle);
 }
